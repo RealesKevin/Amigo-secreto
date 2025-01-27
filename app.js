dc7d3a68b3318ac32c54e8d1031eae9298b219a5
Kevin <kevin.reales2003@gmail.com>
2025-01-27 01:02:30 -0300

    Función para agregar amigos completada

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos=[];
+
+function agregarAmigo() {
+    //captura el valor del campo de entrada
+    let nombre= document.getElementById("amigo").value;
+
+    //verifica la entrada
+    if( nombre.length > 0) {
+        if (amigos.includes(nombre)) {
+            alert("Por favor, ingrese otro nombre; el nombre ya existe.")
+        } else {
+            //actualiza el array de amigos
+            amigos.push(nombre)
+        }
+    } else {
+        alert("Por favor, inserte un nombre.");
+    }
+    //limpiar el campo de entrada
+    limpiarEntrada();
+}
+
+function limpiarEntrada() {
+    document.getElementById("amigo").value = "";
+}
+
