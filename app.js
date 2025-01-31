b7c58405bd58442b9b82551c022186c16f0e7d20
Kevin <kevin.reales2003@gmail.com>
2025-01-31 02:25:41 -0300

    función para sortear los amigos completada

--- app.js
+++ app.js
@@ -33,9 +33,22 @@ function actualizaLista() {
 
     for(let amigo of amigos) {
         let li= document.createElement("li");
-
+        
+        // Convierte el primer carácter del nombre a mayúscula y el resto permanece igual.
         li.textContent = amigo.charAt(0).toUpperCase() + amigo.slice(1);
 
         lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("¡Vamos! Agrega algunos amigos primero y luego podremos hacer el sorteo.");
+    } else {
+        let indiceAleatorio= Math.floor(Math.random() * amigos.length);
+        let amigoSecreto= amigos[indiceAleatorio];
+
+        let resultado= document.getElementById("resultado");
+        resultado.innerHTML = `El amigo secreto sorteado es: ${amigoSecreto}`;
     }
 }
